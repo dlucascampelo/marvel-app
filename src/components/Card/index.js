3cd603c98e52954cc8a21de83d3a61300bd5b60b
Lucas Campelo <dlucascampelo@gmail.com>
2021-03-28 23:20:04 -0300

    Fix: getting  api data

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,13 +4,14 @@ import { ImageBackground, Text, TouchableHighlight, View } from "react-native";
 import styles from "./styles";
 
 export default function Card({ data, navigation }) {
+  console.log(data);
   return (
     <TouchableHighlight>
       <ImageBackground
         style={styles.cardImg}
         resizeMode={"cover"}
         source={{
-          uri: `${data.results.thumbnail.path}.${data.results.thumbnail.extension}`,
+          uri: `${data.thumbnail.path}portrait_small.${data.thumbnail.extension}`,
         }}
       >
         <Text style={styles.cardText}>{data.name}</Text>
